15f71cde1e260a6e79bba38ed892b81b3517ff5d
CPAanalytics <79522568+CPAanalytics@users.noreply.github.com>
2022-01-19 12:02:00 -0600

    Added logic to convert differences to APS standard upload file.

--- overtime_fix/main.py
+++ overtime_fix/main.py
@@ -1,8 +1,11 @@
+# -*- coding:utf-8 -*-
 import os
+from collections import defaultdict
 from pathlib import Path
 from pprint import pprint
 import click as click
 import pandas as pd
+import csv
 
 
 def file_path(filename):
@@ -11,6 +14,38 @@ def file_path(filename):
     file_name = os.path.join(file_path, filename)
     Path(file_path).mkdir(parents=True, exist_ok=True)
     return file_name
+
+
+def create_upload(concat_df):
+    file = csv.writer(open(file_path('upload.csv'), 'w', newline=''))
+    file.writerow(('Employee Name', 'Employee Number', 'Department', '_diff'))
+    for index, rows in concat_df.iterrows():
+        rows = rows[rows != 0]
+        split_employee = str.split(index, '(')
+        employee_name = split_employee[0].strip()
+        employee_id = split_employee[1].strip(')')
+
+        dept_default_dict = defaultdict(int)
+        for income, value in rows.iteritems():
+            if str(income).endswith('_diff'):
+                dept = str(income).split(' ')[1]
+                dept_default_dict[dept] += value
+
+
+        for key,value in dept_default_dict.items():
+            file.writerow((employee_name, int(employee_id), key, value))
+
+def prettify_df(concat_df):
+    # prettify the variances and output to a .txt file.
+    with open(file_path('diff.txt'), 'w') as f:
+        for index, rows in concat_df.iterrows():
+            rows = rows[rows != 0]
+            f.write(str(index) + '\n' * 2)
+            for income, value in rows.iteritems():
+                string = str.ljust(str(income), 30) + ': ' + str(round(value, 2))
+                f.write(string + '\n')
+            f.write('\n')
+    print('Saved ' + file_path('diff.txt'))
 
 
 def fix_horrific_overtime_calc_mess(file_path_org, file_path_recalc, filter_columns_to_keep, filter_columns_to_drop):
@@ -42,7 +77,7 @@ def fix_horrific_overtime_calc_mess(file_path_org, file_path_recalc, filter_colu
     df_actual_from_prod = df_actual_from_prod[df_actual_from_prod.columns.intersection(df_recalculated.columns)]
     df_actual_from_prod = df_actual_from_prod[~df_actual_from_prod['Employee'].isin(missing_employess)]
 
-    # Filter recalculated dataframe to only include the columns that are in the production dataframe`
+    # Filter recalculated dataframe to only include the columns that are in the production dataframe
     df_recalculated = df_recalculated[df_recalculated.columns.intersection(df_actual_from_prod.columns)]
     df_recalculated = df_recalculated[~df_recalculated['Employee'].isin(missing_employess_recalc)]
 
@@ -113,17 +148,9 @@ def fix_horrific_overtime_calc_mess(file_path_org, file_path_recalc, filter_colu
     concat_df.to_csv(file_path('diff.csv'))
     print('Saved ' + file_path('diff.csv'))
 
-    # prettify the variances and output to a .txt file.
-    with open(file_path('diff.txt'), 'w') as f:
-        for index, rows in concat_df.iterrows():
-            rows = rows[rows != 0]
-            f.write(str(index) + '\n' * 2)
-            for income, value in rows.iteritems():
-                string = str.ljust(str(income), 30) + ': ' + str(round(value, 2))
-                f.write(string + '\n')
-            f.write('\n')
-    print('Saved ' + file_path('diff.txt'))
 
+    create_upload(concat_df)
+    prettify_df(concat_df)
 
 @click.command()
 @click.option('--prod', required=True, help='Production file to compare')
@@ -136,5 +163,5 @@ def overtime_fix(prod, recalc, keep, drop):
 
 if __name__ == "__main__":
     overtime_fix(
-        ['--prod', '../csv_files/prod.csv', '--recalc', '../csv_files/recalc.csv', '--keep', 'Overtime ', '--drop',
+        ['--prod', './csv_files/prod.csv', '--recalc', './csv_files/recalc.csv', '--keep', 'Overtime ', '--drop',
          'Overtime Units'])
